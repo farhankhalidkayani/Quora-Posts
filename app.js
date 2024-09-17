09a58ff1022e4357eba547d07d6e2bd5e4420487
Farhan Khalid <rj.farhan4232@gmail.com>
2024-09-17 17:22:13 +0500

    Completed all REST API routes and added styling to the templates

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
@@ -29,6 +32,9 @@ let posts = [
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
 });
+app.get("/posts/new", (req, res) => {
+  res.render("form.ejs");
+});
 app.get("/posts/:id", (req, res) => {
   const { id } = req.params;
   let post = posts.find((p) => Number(id) === p.id);
@@ -38,13 +44,40 @@ app.get("/posts/:id", (req, res) => {
   }
   res.render("post.ejs", { post });
 });
-app.get("/posts/new", (req, res) => {
-  res.render("form.ejs");
-});
+
 app.post("/posts", (req, res) => {
   const id = ++idcount;
   const { username, content } = req.body;
   posts.push({ id, username, content });
   res.redirect("/posts");
 });
+app.patch("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const newContent = req.body.content;
+  let post = posts.find((p) => Number(id) === p.id);
+  if (!post) {
+    res.render("404.ejs");
+  }
+  post.content = newContent;
+  res.render("post.ejs", { post });
+});
+app.get("/posts/:id/edit", (req, res) => {
+  const { id } = req.params;
+  let post = posts.find((p) => Number(id) === p.id);
+  if (!post) {
+    res.render("404.ejs");
+  }
+
+  res.render("edit.ejs", { post });
+});
+app.delete("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  let post = posts.find((p) => Number(id) === p.id);
+  if (!post) {
+    res.render("404.ejs");
+  }
+  posts = posts.filter((p) => Number(id) !== p.id);
+  res.redirect("/posts");
+});
+
 app.listen(3000);
