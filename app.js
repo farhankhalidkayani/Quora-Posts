d5e9deb9f8383906f74e58131f50e76b1154bea1
Farhan Khalid <rj.farhan4232@gmail.com>
2024-09-17 15:14:19 +0500

    Implemented Post Detailes Route

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+let idcount = 0;
 let posts = [
   {
+    id: ++idcount,
     username: "Farhan",
     content: "I am Learning Backend Development with JavaScript",
   },
   {
+    id: ++idcount,
     username: "Mithu",
     content: "I am Learning Backend Development with Python",
   },
   {
+    id: ++idcount,
     username: "Zohaib",
     content: "I am Learning Frontent Development with ReactJs",
   },
@@ -26,12 +29,22 @@ let posts = [
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
 });
+app.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  let post = posts.find((p) => Number(id) === p.id);
+
+  if (!post) {
+    res.render("404.ejs");
+  }
+  res.render("post.ejs", { post });
+});
 app.get("/posts/new", (req, res) => {
   res.render("form.ejs");
 });
 app.post("/posts", (req, res) => {
+  const id = ++idcount;
   const { username, content } = req.body;
-  posts.push({ username, content });
+  posts.push({ id, username, content });
   res.redirect("/posts");
 });
 app.listen(3000);
